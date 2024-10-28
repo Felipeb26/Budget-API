4738bbcc8cf134e7f53e26581bfc3e3ded3d3237
FelipeBSilva <felipe.silva@grupomultiplica.com.br>
2024-10-28 18:52:18 -0300

    ajustando atualização manual

--- src/main/java/com/batsworks/domain/entity/BillEntity.java
+++ src/main/java/com/batsworks/domain/entity/BillEntity.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "bills")
@@ -19,4 +21,79 @@ public class BillEntity extends AbstractEntity<BillEntity> {
     private BigDecimal value;
     private LocalDate data;
 
+    public BillEntity(UUID id, LocalDateTime createdDate, LocalDateTime updateDate, String className, UserEntity userEntity, String name, BigDecimal value, LocalDate data) {
+        super(id, createdDate, updateDate, className);
+        this.userEntity = userEntity;
+        this.name = name;
+        this.value = value;
+        this.data = data;
+    }
+
+    public BillEntity(UserEntity userEntity, String name, BigDecimal value, LocalDate data) {
+        this.userEntity = userEntity;
+        this.name = name;
+        this.value = value;
+        this.data = data;
+    }
+
+    public BillEntity() {
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public static class Builder {
+        private UserEntity userEntity;
+        private String name;
+        private BigDecimal value;
+        private LocalDate data;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setValue(BigDecimal value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder setData(LocalDate data) {
+            this.data = data;
+            return this;
+        }
+
+        public BillEntity build() {
+            return new BillEntity(this.userEntity, this.name, this.value, this.data);
+        }
+    }
 }
